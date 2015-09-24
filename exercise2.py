48499932086e7b208e24a44f88410a908fc39854
ecanning <erin.canning@mail.utoronto.ca>
2015-09-24 12:40:48 -0400

    function added questions for further notes

--- exercise2.py
+++ exercise2.py
@@ -14,26 +14,30 @@ __license__ = "MIT License"
 
 
 def name_that_shape():
-    shape_sides = input("How many sides does your shape have? ")
+    shape_sides = raw_input("How many sides does your shape have? ")
     shape_sides = int(shape_sides)
     if shape_sides == 3:
-        print("That is a triangle")
+        print("Your shape has %d sides! That is a triangle.") %shape_sides
     elif shape_sides == 4:
-        print ("That is a rectangle")
+        print ("Your shape has %d sides! That is a rectangle.") %shape_sides
     elif shape_sides == 5:
-        print ("That is a pentagon")
+        print ("Your shape has %d sides! That is a pentagon.") %shape_sides
     elif shape_sides == 6:
-        print ("That is a hexagon")
+        print ("Your shape has %d sides! That is a hexagon.") %shape_sides
     elif shape_sides == 7:
-        print ("That is a septagon")
+        print ("Your shape has %d sides! That is a septagon.") %shape_sides
     elif shape_sides == 8:
-        print ("That is a octogon")
+        print ("Your shape has %d sides! That is a octogon.") %shape_sides
     elif shape_sides == 9:
-        print ("That is a nonagon")
+        print ("Your shape has %d sides! That is a nonagon.") %shape_sides
     elif shape_sides == 10:
-        print ("That is a decagon")
+        print ("Your shape has %d sides! That is a decagon.") %shape_sides
     else:
-        print ("Error")
+        if shape_sides < 3:
+            print ("Error! That is not enough sides. Your shape must have at least 3 sides.")
+        elif shape_sides > 10:
+            print ("Error! That is too many sides. Your shape must have less than 10 sides.")
+
 
     """
     For a given number of sides in a regular polygon, returns the shape name
@@ -43,6 +47,11 @@ def name_that_shape():
     Expected Outputs: str from input (name of object)
 
     Errors: if input >3-10<, produce error message
+
+    !!Error message for if input is str not int?
+        print ("raw_input is not a number! Please enter a number.") - loop back to beginning again?
+    !!Is there a way to simplify this code?
+    !!If input is <3-10<, loop back to beginning of code?
 
     """
 
