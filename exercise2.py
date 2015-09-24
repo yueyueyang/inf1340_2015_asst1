444970c875a31ca94220208f9f038cc8eff94cf8
ecanning <erin.canning@mail.utoronto.ca>
2015-09-24 16:54:00 -0400

    function list changed to list with index calls value error for input strings,symbols, 0, negative int while loop to prompt input when incorrect until correct input reached

--- exercise2.py
+++ exercise2.py
@@ -12,25 +12,31 @@ __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
-shapes = {0:" ", 1:"point", 2:"line", 3:"triangle", 4:"quadrangle", 5:"pentagon", 6:"hexagon", 7:"septagon", 8:"octogon", 9:"nonagon", 10:"decagon"}
+
+shapes = ["nothing", "point", "line", "triangle", "quadrangle", "pentagon", "hexagon", "septagon", "octogon", "nonagon", "decagon"]
+
 def name_that_shape():
-    shape_sides = raw_input("How many sides does your shape have? ")
-    if len(shape_sides) == 0:
-        print ("You need to enter a number. How many sides does your shape have?")
-    else:
+    while True:
         try:
-            shape_sides = int(shape_sides)
+            shape_sides = int(raw_input("How many sides does your shape have? "))
         except ValueError:
-            print ("That is a word, not a number! You need to enter a number.")
-            #loop here back to raw_input so does not continue with word and count number of letters
-    if 3 <= shape_sides <= 10:
-        print "Your shape has %d sides! That is a %s." %(shape_sides, shapes[shape_sides])
-    elif shape_sides == 0:
-        print("Error! That is no sides at all. Your shapes must have at least 3 sides.")
-    elif 1 <= shape_sides < 3:
-        print ("Error! That is not enough sides. Your shape must have at least 3 sides.")
-    elif 10 < shape_sides:
-        print ("Error! That is too many sides. Your shape must have less than 10 sides.")
+            print ("Sorry, I didn't catch that. Please enter a number.")
+            continue
+        if shape_sides < 0:
+            print ("Error! A shape cannot have negative sides. A shape must have at least three sides.")
+            continue
+        elif shape_sides == 0:
+            print("Error! That is no sides at all. Your shapes must have at least 3 sides.")
+            continue
+        elif 1 <= shape_sides < 3:
+            print ("Error! That is not enough sides. Your shape must have at least 3 sides.")
+            continue
+        elif 10 < shape_sides:
+            print ("Error! That is too many sides. Your shape must have less than 10 sides.")
+            continue
+        else:
+            print "Your shape has %d sides! That is a %s." %(shape_sides, shapes[shape_sides])
+            break
 
 name_that_shape()
 
@@ -41,7 +47,4 @@ name_that_shape()
 #Expected Outputs: str from input (name of object)
 
 #Errors: if input >3-10<, produce error message
-
-#!!Error message for if input is str not int? - loop back to beginning again?
-#!!If input is <3-10<, loop back to beginning of code?
 
